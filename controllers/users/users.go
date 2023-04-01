3eb2e964613c875060c40dc31bc2be17b1c7bc1a
Evgenii Myshevskii <e.g.myshevskiy@gmail.com>
2023-04-01 21:21:26 +0000

    end of 8 video
    add usersDB operations mock

--- controllers/users/users.go
+++ controllers/users/users.go
@@ -5,6 +5,7 @@ import (
 	"main/services"
 	"main/utils/errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,7 +14,7 @@ func CreateUser(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		//TODO: handle json error
-		restErr := errors.NewBadRequest("invalid json body")
+		restErr := errors.NewBadRequestError("invalid json body")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -27,5 +28,17 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implenent me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
