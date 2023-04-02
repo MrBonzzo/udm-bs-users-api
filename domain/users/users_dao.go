064ecc2fa6ef57c9e102c6fbb999126cb4f3ceeb
Evgenii Myshevskii <e.g.myshevskiy@gmail.com>
2023-04-02 13:26:55 +0300

    end of 11 video
    save created user to mysql database

--- domain/users/users_dao.go
+++ domain/users/users_dao.go
@@ -5,6 +5,12 @@ import (
 	"main/datasources/mysql/users_db"
 	"main/utils/date_utils"
 	"main/utils/errors"
+	"strings"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
 var (
@@ -12,16 +18,31 @@ var (
 )
 
 func (user *User) Save() *errors.RestErr {
-	current := userDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
-	}
-	user.DateCreated = date_utils.GetNowStirng()
 
-	userDB[user.Id] = user
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+
+	user.DateCreated = date_utils.GetNowStirng()
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(
+				fmt.Sprintf("email %s already exists", user.Email),
+			)
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", err.Error()),
+		)
+	}
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+
+	user.Id = userId
 	return nil
 }
 
